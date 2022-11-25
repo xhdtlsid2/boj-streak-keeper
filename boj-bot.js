96b036ef9e0cbda670ff6ba967c9428fb737c1a4
Jaeheon <xhdtlsid2@gmail.com>
2022-11-25 22:11:57 +0900

    changed refresh address, some minor changes

--- boj-bot.js
+++ boj-bot.js
@@ -9,23 +9,21 @@ const oneDayInMilliseconds = 1000 * 60 * 60 * 24;
 const refreshInterval = 1000 * 60 * 25;
 
 /**
-* logging in by entering id/pw.
-* won't work when recaptcha appears.
-* so use at your own risk.
-*
-*	async function directLogin(driver){
-*		const id = "", password = "";
-*
-*		const loginForm = await driver.findElement(By.id("login_form"));
-*		await loginForm.findElement(By.name("login_user_id")).sendKeys(id);
-*		await loginForm.findElement(By.name("login_password")).sendKeys(password);
-*		// await loginForm.findElement(By.name("auto_login")).click();
-*		await loginForm.findElement(By.id("submit_button")).click();
-*		
-*		// wait for POST and GET done
-*		await setTimeout(10000);
-*	}
-*/
+ * Logging in by entering id/pw.
+ * Won't work when recaptcha appears.
+ * So use at your own risk.
+ */
+async function directLogin(driver){
+	const id = "", password = "";
+	const loginForm = await driver.findElement(By.id("login_form"));
+	await loginForm.findElement(By.name("login_user_id")).sendKeys(id);
+	await loginForm.findElement(By.name("login_password")).sendKeys(password);
+	// await loginForm.findElement(By.name("auto_login")).click();
+	await loginForm.findElement(By.id("submit_button")).click();
+	
+	// wait for POST and GET done
+	await setTimeout(10000);
+}
 
 async function autoSubmit(){
 	let driver = await new Builder()
@@ -34,8 +32,14 @@ async function autoSubmit(){
 		.build();
 
 	//login
-	await driver.get("https://acmicpc.net/");
+	await driver.get("https://acmicpc.net/status");
 	await driver.manage().deleteCookie("OnlineJudge");
+	// to set custom cookie value:
+	// 1. go to acmicpc.net
+	// 2. if you are already loggin in, logout
+	// 3. set the OnlineJudge cookie value to whatever you want
+	// 4. login!
+	// then you will be able to use your custom cookie value
 	const cookie = fs.readFileSync("./cookie.txt").toString();
 	await driver.manage().addCookie({name: "OnlineJudge", value: cookie, httpOnly: true});
 
@@ -63,7 +67,7 @@ async function autoSubmit(){
 		}
 		else if(currentTime - lastRefreshTime > refreshInterval){
 			lastRefreshTime = currentTime;
-			await driver.get("https://acmicpc.net/");
+			await driver.get("https://acmicpc.net/status");		//contains no big images or ads; faster load
 		}
 	}
 }
